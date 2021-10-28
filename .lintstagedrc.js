fbe56079995e5d40a7d65ec96e93a4999a02e3a5
AssisrMatheus <assisr.matheus@gmail.com>
2021-10-27 21:27:35 -0300

    chore: setting up lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  '*': 'prettier --ignore-unknown --write',
+  '**/*.{js,jsx,ts,tsx,html,css,md,mdx}': (filenames) => {
+    const relativeFilenames = filenames
+      // Completely removes the current path(at __dirname) to an absolute path
+      // By replacing the initial part of the string to nothing
+      .map((x) => path.normalize(x).replace(path.resolve(__dirname), ''))
+      .join(' --file ');
+    return `next lint --fix --file ${relativeFilenames}`;
+  }
+};
