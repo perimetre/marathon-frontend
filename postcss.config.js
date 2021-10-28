143d2ddf7856d6bcd18c255e634d109109ce2f4f
AssisrMatheus <assisr.matheus@gmail.com>
2021-10-27 22:04:08 -0300

    chore: added postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
+    'postcss-import': {}, // Import must come before tailwind
     tailwindcss: {},
+    'postcss-nested': {},
+    'postcss-combine-media-query': {}, // Media query must come before duplicated-selectors
+    'postcss-combine-duplicated-selectors': {},
     autoprefixer: {}
   }
 };
