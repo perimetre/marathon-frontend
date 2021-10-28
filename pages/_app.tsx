ecc464f4dbec814ecaa28fabbcc6df341507dbed
AssisrMatheus <assisr.matheus@gmail.com>
2021-10-28 08:20:45 -0300

    chore: added fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
+import '../styles/globals.css';
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
