2664971a2c548f4c26604758d9985d039fc8b78e
AssisrMatheus <assisr.matheus@gmail.com>
2021-10-29 16:29:42 -0300

    fix: setup git lfs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   'src/**/*': 'prettier --ignore-unknown --write',
-  './src/**/*.+(js|jsx|ts|tsx)': (filenames) => {
+  'src/**/*.+(js|jsx|ts|tsx)': (filenames) => {
     const relativeFilenames = filenames
       // Completely removes the current path(at __dirname) to an absolute path
       // By replacing the initial part of the string to nothing
@@ -11,6 +11,6 @@ module.exports = {
     return `next lint --fix --file ${relativeFilenames}`;
   },
   // Make sure to keep forward slash on stylelint
-  './src/**/*.+(css|scss)': 'stylelint --fix',
+  'src/**/*.+(css|scss)': 'stylelint --fix',
   'src/**/*.ts?(x)': 'eslint --fix --plugin tsc --rule \'tsc/config: [2, {configFile: "./tsconfig.json"}]\''
 };
