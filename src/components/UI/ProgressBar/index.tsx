2f7d90cea5f9849e7ba36a5e3858e571f0ffd4f0
AssisrMatheus <assisr.matheus@gmail.com>
2021-11-01 11:34:19 -0300

    feat: adds default border-radius

--- src/components/UI/ProgressBar/index.tsx
+++ src/components/UI/ProgressBar/index.tsx
@@ -15,14 +15,14 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progress, className, color })
     <div
       className={classnames(
         color,
-        'w-full overflow-hidden border-2 p-1 border-mui-placeholder-color h-6 rounded',
+        'w-full overflow-hidden border-2 p-1 border-mui-placeholder-color h-6 mui-border-radius',
         className
       )}
     >
       <div className="relative w-full h-full overflow-hidden">
         <div
           style={{ width: `${progress}%` }}
-          className="absolute top-0 bottom-0 rounded transition-all bg-mui-placeholder-color"
+          className="absolute top-0 bottom-0 mui-border-radius transition-all bg-mui-placeholder-color"
           data-progress={progress}
         />
       </div>
