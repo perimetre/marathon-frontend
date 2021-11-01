483e97d63d7baffa04832e0bdda23d5cfb39250e
AssisrMatheus <assisr.matheus@gmail.com>
2021-11-01 11:10:11 -0300

    fix: fixes domain on next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: { domains: ['marathon-media-dev01.nyc3.digitaloceanspaces.com'] },
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['default', 'en', 'fr'],
+    locales: ['default', 'en' /*'fr'*/],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     // Ref, fix: https://github.com/vercel/next.js/discussions/18419#discussioncomment-1561577
