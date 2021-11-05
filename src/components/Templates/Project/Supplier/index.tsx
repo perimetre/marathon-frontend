a26213f09c9c9c845944abde0b8f3112695213b3
AssisrMatheus <assisr.matheus@gmail.com>
2021-11-05 15:15:52 -0300

    feat: updated title

--- src/components/Templates/Project/Supplier/index.tsx
+++ src/components/Templates/Project/Supplier/index.tsx
@@ -1,5 +1,5 @@
 import { Form, Formik } from 'formik';
-import { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { GetSlideSupplierByCollectionQuery } from '../../../../apollo/generated/graphql';
 import { Select } from '../../../UI/Form/Select';
 import * as yup from 'yup';
@@ -10,6 +10,7 @@ import Skeleton from '../../../UI/Skeleton';
 import ErrorMessage from '../../../UI/ErrorMessage';
 import SkeletonImage from '../../../UI/SkeletonImage';
 import classNames from 'classnames';
+import Head from 'next/head';
 
 export type SupplierTemplateProps = {
   data?: GetSlideSupplierByCollectionQuery;
@@ -71,117 +72,126 @@ const SupplierTemplate: React.FC<SupplierTemplateProps> = ({
   }, [data, initialValue]);
 
   return (
-    <Formik
-      initialValues={{
-        slide: initialValue?.slide || null,
-        depth: initialValue?.depth || '',
-        model: initialValue?.model || ''
-      }}
-      onSubmit={onSubmit}
-      validationSchema={schema}
-      validateOnMount
-    >
-      {({ values, setFieldValue, isValid }) => (
-        <Form>
-          <ProjectCreationTemplate
-            step={4}
-            title={intl.formatMessage({ id: 'project.supplierTitle' })}
-            disableNext={!isValid}
-            handlePrev={() => router.push('/project/finish', '/project/finish')}
-          >
-            {error && (
-              <div className="container flex items-center justify-center mx-auto mt-8">
-                <ErrorMessage error={`serverErrors.${error}`} handleTryAgain={handleTryAgain} />
-              </div>
-            )}
-            <div className="container flex mx-auto mt-16 mb-16">
-              {loading ? (
-                <div className="flex flex-col flex-1 py-6 pr-6 border-r border-gray-300 gap-6">
-                  <Skeleton className="w-full h-24" />
-                  <Skeleton className="w-full h-24" />
-                  <Skeleton className="w-full h-24" />
-                  <Skeleton className="w-full h-24" />
+    <>
+      <Head>
+        <title>
+          {`${intl.formatMessage({ id: 'project.supplierTitle' })} | ${intl.formatMessage({
+            id: 'title'
+          })}`}
+        </title>
+      </Head>
+      <Formik
+        initialValues={{
+          slide: initialValue?.slide || null,
+          depth: initialValue?.depth || '',
+          model: initialValue?.model || ''
+        }}
+        onSubmit={onSubmit}
+        validationSchema={schema}
+        validateOnMount
+      >
+        {({ values, setFieldValue, isValid }) => (
+          <Form>
+            <ProjectCreationTemplate
+              step={4}
+              title={intl.formatMessage({ id: 'project.supplierHeader' })}
+              disableNext={!isValid}
+              handlePrev={() => router.push('/project/finish', '/project/finish')}
+            >
+              {error && (
+                <div className="container flex items-center justify-center mx-auto mt-8">
+                  <ErrorMessage error={`serverErrors.${error}`} handleTryAgain={handleTryAgain} />
                 </div>
-              ) : (
-                <div className="flex flex-col flex-1 py-6 pr-6 border-r border-gray-300 gap-6">
-                  {data?.slideSuppliers.map((slide) => {
-                    const active = values.slide === slide.id;
-                    return (
-                      <div
-                        key={`type-card-${slide.id}`}
-                        className={classNames(
-                          'flex bg-white rounded-md shadow-sm border-2 transition-all hover:-translate-y-1 hover:shadow-lg duration-75',
-                          active && 'border-mui-primary'
-                        )}
-                        role="button"
-                        aria-hidden="true"
-                        onClick={() => {
-                          setFieldValue('model', '');
+              )}
+              <div className="container flex mx-auto mt-16 mb-16">
+                {loading ? (
+                  <div className="flex flex-col flex-1 py-6 pr-6 border-r border-gray-300 gap-6">
+                    <Skeleton className="w-full h-24" />
+                    <Skeleton className="w-full h-24" />
+                    <Skeleton className="w-full h-24" />
+                    <Skeleton className="w-full h-24" />
+                  </div>
+                ) : (
+                  <div className="flex flex-col flex-1 py-6 pr-6 border-r border-gray-300 gap-6">
+                    {data?.slideSuppliers.map((slide) => {
+                      const active = values.slide === slide.id;
+                      return (
+                        <div
+                          key={`type-card-${slide.id}`}
+                          className={classNames(
+                            'flex bg-white rounded-md shadow-sm border-2 transition-all hover:-translate-y-1 hover:shadow-lg duration-75',
+                            active && 'border-mui-primary'
+                          )}
+                          role="button"
+                          aria-hidden="true"
+                          onClick={() => {
+                            setFieldValue('model', '');
+                            setFieldValue('depth', '');
+                            setFieldValue('slide', slide.id);
+                            setSlide(slide.id);
+                          }}
+                        >
+                          <div className="flex items-center justify-center w-64 h-full bg-mui-gray-300">
+                            {slide.thumbnailUrl && (
+                              <SkeletonImage
+                                className="object-contain"
+                                width={140}
+                                height={58}
+                                src={slide.thumbnailUrl}
+                                alt={slide.name}
+                              />
+                            )}
+                          </div>
+                          <div className="flex items-center p-14">
+                            <h2 className="text-xl font-bold uppercase">{slide.name}</h2>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+                {loading ? (
+                  <div className="flex flex-col flex-1 pt-6 pl-6 lg:pl-16 gap-6">
+                    <Skeleton className="h-16 w-96" />
+                    <Skeleton className="h-16 w-96" />
+                  </div>
+                ) : (
+                  <div className="flex flex-col flex-1 pt-6 pl-6 lg:pl-16 gap-6">
+                    <label className="block w-full max-w-xl text-left">
+                      <Select
+                        name="model"
+                        label="Model"
+                        onChange={(e) => {
                           setFieldValue('depth', '');
-                          setFieldValue('slide', slide.id);
-                          setSlide(slide.id);
+                          setModel(Number(e.target.value));
                         }}
                       >
-                        <div className="flex items-center justify-center w-64 h-full bg-mui-gray-300">
-                          {slide.thumbnailUrl && (
-                            <SkeletonImage
-                              className="object-contain"
-                              width={140}
-                              height={58}
-                              src={slide.thumbnailUrl}
-                              alt={slide.name}
-                            />
-                          )}
-                        </div>
-                        <div className="flex items-center p-14">
-                          <h2 className="text-xl font-bold uppercase">{slide.name}</h2>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              )}
-              {loading ? (
-                <div className="flex flex-col flex-1 pt-6 pl-6 lg:pl-16 gap-6">
-                  <Skeleton className="h-16 w-96" />
-                  <Skeleton className="h-16 w-96" />
-                </div>
-              ) : (
-                <div className="flex flex-col flex-1 pt-6 pl-6 lg:pl-16 gap-6">
-                  <label className="block w-full max-w-xl text-left">
-                    <Select
-                      name="model"
-                      label="Model"
-                      onChange={(e) => {
-                        setFieldValue('depth', '');
-                        setModel(Number(e.target.value));
-                      }}
-                    >
-                      <option key="">Select Option</option>
-                      {models?.map((model) => (
-                        <option key={model.id} value={model.id}>
-                          {model.product}
-                        </option>
-                      ))}
-                    </Select>
-                  </label>
-                  <label className="block w-full max-w-xl text-left">
-                    <Select name="depth" label="Depth">
-                      <option key="">Select Option</option>
-                      {depths?.map((depth) => (
-                        <option key={depth.id} value={depth.id}>
-                          {depth.display}
-                        </option>
-                      ))}
-                    </Select>
-                  </label>
-                </div>
-              )}
-            </div>
-          </ProjectCreationTemplate>
-        </Form>
-      )}
-    </Formik>
+                        <option key="">Select Option</option>
+                        {models?.map((model) => (
+                          <option key={model.id} value={model.id}>
+                            {model.product}
+                          </option>
+                        ))}
+                      </Select>
+                    </label>
+                    <label className="block w-full max-w-xl text-left">
+                      <Select name="depth" label="Depth">
+                        <option key="">Select Option</option>
+                        {depths?.map((depth) => (
+                          <option key={depth.id} value={depth.id}>
+                            {depth.display}
+                          </option>
+                        ))}
+                      </Select>
+                    </label>
+                  </div>
+                )}
+              </div>
+            </ProjectCreationTemplate>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- src/components/Templates/Project/Type/index.tsx
+++ src/components/Templates/Project/Type/index.tsx
@@ -1,5 +1,5 @@
 import { Form, Formik } from 'formik';
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import { GetTypeQuery } from '../../../../apollo/generated/graphql';
 import Card from '../../../UI/Card';
@@ -7,6 +7,7 @@ import * as yup from 'yup';
 import ProjectCreationTemplate from '../../ProjectCreation';
 import ErrorMessage from '../../../UI/ErrorMessage';
 import Skeleton from '../../../UI/Skeleton';
+import Head from 'next/head';
 
 export type TypeTemplateProps = {
   data?: GetTypeQuery;
@@ -37,50 +38,59 @@ const TypeTemplate: React.FC<TypeTemplateProps> = ({
   );
 
   return (
-    <Formik
-      initialValues={{ type: initialValue?.type || null }}
-      onSubmit={onSubmit}
-      validationSchema={schema}
-      validateOnMount
-    >
-      {({ values, isValid, setFieldValue }) => (
-        <Form>
-          <ProjectCreationTemplate
-            step={1}
-            title={intl.formatMessage({ id: 'project.typeTitle' })}
-            disablePrev
-            disableNext={!isValid}
-          >
-            {error && (
-              <div className="container flex items-center justify-center mx-auto mt-8">
-                <ErrorMessage error={`serverErrors.${error}`} handleTryAgain={handleTryAgain} />
-              </div>
-            )}
-            {loading ? (
-              <div className="container flex flex-wrap justify-center mx-auto my-16 gap-16">
-                <Skeleton className="h-96 w-96" />
-                <Skeleton className="h-96 w-96" />
-                <Skeleton className="h-96 w-96" />
-              </div>
-            ) : (
-              <div className="container flex flex-wrap justify-center mx-auto my-16 gap-16">
-                {data?.types.map((type) => (
-                  <Card
-                    key={`type-card-${type.id}`}
-                    active={values.type === type.id}
-                    imageClassName="object-cover object-center"
-                    onClick={() => setFieldValue('type', type.id)}
-                    image={type.thumbnailUrl || ''}
-                    title={type.name}
-                    description={type.description}
-                  />
-                ))}
-              </div>
-            )}
-          </ProjectCreationTemplate>
-        </Form>
-      )}
-    </Formik>
+    <>
+      <Head>
+        <title>
+          {`${intl.formatMessage({ id: 'project.typeTitle' })} | ${intl.formatMessage({
+            id: 'title'
+          })}`}
+        </title>
+      </Head>
+      <Formik
+        initialValues={{ type: initialValue?.type || null }}
+        onSubmit={onSubmit}
+        validationSchema={schema}
+        validateOnMount
+      >
+        {({ values, isValid, setFieldValue }) => (
+          <Form>
+            <ProjectCreationTemplate
+              step={1}
+              title={intl.formatMessage({ id: 'project.typeHeader' })}
+              disablePrev
+              disableNext={!isValid}
+            >
+              {error && (
+                <div className="container flex items-center justify-center mx-auto mt-8">
+                  <ErrorMessage error={`serverErrors.${error}`} handleTryAgain={handleTryAgain} />
+                </div>
+              )}
+              {loading ? (
+                <div className="container flex flex-wrap justify-center mx-auto my-16 gap-16">
+                  <Skeleton className="h-96 w-96" />
+                  <Skeleton className="h-96 w-96" />
+                  <Skeleton className="h-96 w-96" />
+                </div>
+              ) : (
+                <div className="container flex flex-wrap justify-center mx-auto my-16 gap-16">
+                  {data?.types.map((type) => (
+                    <Card
+                      key={`type-card-${type.id}`}
+                      active={values.type === type.id}
+                      imageClassName="object-cover object-center"
+                      onClick={() => setFieldValue('type', type.id)}
+                      image={type.thumbnailUrl || ''}
+                      title={type.name}
+                      description={type.description}
+                    />
+                  ))}
+                </div>
+              )}
+            </ProjectCreationTemplate>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
