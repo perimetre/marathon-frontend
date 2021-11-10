064773a7bf5bf843a8ee02ad3da18261966a676a
Alessandro Macanha <alessandromacanha@gmail.com>
2021-11-10 14:31:45 -0300

    fix: change on options hook

--- src/components/Elements/SidebarModuleDetail/index.tsx
+++ src/components/Elements/SidebarModuleDetail/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { ModuleDataFragment } from '../../../apollo/generated/graphql';
 import ModuleButtonImage from '../ModuleButtonImage';
 import SidebarModuleAddons from '../SidebarModuleAddons';
@@ -8,12 +8,14 @@ type SidebarModuleDetailProps = {
 };
 
 const SidebarModuleDetail: React.FC<SidebarModuleDetailProps> = ({ module }) => {
+  const options = useMemo(() => {
+    return [...(module.rules?.extensions?.options || []), ...(module.rules?.rules?.options || [])];
+  }, [module.rules]);
+
   return (
     <div className="flex flex-col h-full mr-4">
       <ModuleButtonImage module={module} />
-      {module.rules?.extensions?.options && module.rules.extensions.options.length > 0 && (
-        <SidebarModuleAddons options={module.rules.extensions.options} />
-      )}
+      {options && options.length > 0 && <SidebarModuleAddons options={options} />}
     </div>
   );
 };
