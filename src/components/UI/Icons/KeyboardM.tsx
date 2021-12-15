1ac6955a244ad3d1372380af5a4db0a6ed4929c7
Alessandro Macanha <alessandromacanha@gmail.com>
2021-12-15 09:11:22 -0300

    fix: change icons default color to currentColor

--- src/components/UI/Icons/KeyboardM.tsx
+++ src/components/UI/Icons/KeyboardM.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const KeyboardM: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect x="4" y="4" width="16" height="16" rx="3" stroke="white" strokeWidth="1.4" />
+    <rect x="4" y="4" width="16" height="16" rx="3" stroke="currentColor" strokeWidth="1.4" />
     <path
       d="M9.44336 7.46875L12.2324 14.4297L15.0215 7.46875H16.4805V16H15.3555V12.6777L15.4609 9.0918L12.6602 16H11.7988L9.00391 9.10938L9.11523 12.6777V16H7.99023V7.46875H9.44336Z"
-      fill="white"
+      fill="currentColor"
     />
   </svg>
 );
--- src/components/UI/Icons/MouseScroll.tsx
+++ src/components/UI/Icons/MouseScroll.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MouseScroll: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg width="22" height="22" viewBox="0 0 530 874" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect x="20" y="20" width="490" height="834" rx="229" stroke="white" strokeWidth="40" />
-    <rect x="201" y="119" width="128" height="247" rx="64" fill="white" />
+    <rect x="20" y="20" width="490" height="834" rx="229" stroke="currentColor" strokeWidth="40" />
+    <rect x="201" y="119" width="128" height="247" rx="64" fill="currentColor" />
   </svg>
 );
 
